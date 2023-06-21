fd6c730c632378ac3a1e5e112f847279a2869fd4
Isaias Dominguez <isaiasdr21@gmail.com>
2023-06-20 22:03:46 -0400

    UPDATE: finish providers and show list movies

---
+++ lib/presentation/providers/movies/movies_repository_provider.dart
@@ -0,0 +1,9 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import 'package:cinemapedia/infraestructure/datasources/moviedb_datasource.dart';
+import 'package:cinemapedia/infraestructure/repositories/movie_repository_impl.dart';
+
+//inmutable
+final movieRepositoryProvider = Provider((ref) {
+  return MovieRepositoryImpl(MovieDbDatasource());
+});
